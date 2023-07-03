dd87b2df0e85afc19f7176f4159fcfd520b04d20
alexandruiradu <alexandruradu4288@outlook.com>
2023-07-03 20:51:11 +0300

    Create candle

--- candle.store/src/main/java/com/candle/store/controller/CandleController.java
+++ candle.store/src/main/java/com/candle/store/controller/CandleController.java
@@ -1,13 +1,21 @@
 package com.candle.store.controller;
 
+import com.candle.store.dto.CandleDto;
 import com.candle.store.entity.Candle;
 import com.candle.store.service.CandleService;
+import com.candle.store.service.CandleValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -15,6 +23,9 @@ public class CandleController {
 
     @Autowired
     private CandleService candleService;
+
+    @Autowired
+    private CandleValidator candleValidator;
 
 
     @GetMapping("/")
@@ -39,6 +50,32 @@ public class CandleController {
         model.addAttribute("candles", candles);
 
         return "candles";
+    }
+
+    @GetMapping("/candle")
+    public String viewCandleForm(Model model) {
+        CandleDto candleDto = new CandleDto();
+        model.addAttribute("candle", candleDto);
+
+        return "candle";
+    }
+
+    @PostMapping("/candle/save")
+    public String saveCandle(
+            @ModelAttribute("candle") CandleDto candleDto,
+            BindingResult bindingResult,
+            Model model,
+            @RequestParam("coverImage") MultipartFile file
+    ) throws IOException {
+        candleValidator.validate(candleDto, bindingResult);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("candle", candleDto);
+            return "candle";
+        }
+        candleService.saveCandle(candleDto, file);
+        List<Candle> candles = candleService.getAllCandles();
+        model.addAttribute("candles", candles);
+        return "redirect:/candles";
     }
 
 
--- candle.store/src/main/java/com/candle/store/service/CandleService.java
+++ candle.store/src/main/java/com/candle/store/service/CandleService.java
@@ -1,10 +1,14 @@
 package com.candle.store.service;
 
+import com.candle.store.dto.CandleDto;
 import com.candle.store.entity.Candle;
+import com.candle.store.mapper.CandleMapper;
 import com.candle.store.repository.CandleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -13,7 +17,15 @@ public class CandleService {
     @Autowired
     private CandleRepository candleRepository;
 
+    @Autowired
+    private CandleMapper candleMapper;
+
     public List<Candle> getAllCandles() {
         return candleRepository.findAll();
+    }
+
+    public void saveCandle(CandleDto candleDto, MultipartFile file) throws IOException {
+        Candle candle = candleMapper.mapper(candleDto, file);
+        candleRepository.save(candle);
     }
 }
